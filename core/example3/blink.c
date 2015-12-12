2474432ec05e734d58ce96752d29fb96ce31baca
cyberSJ <sungjoonkim4.0@gmail.com>
2015-12-12 14:15:15 -0500

    use_pickit2 : Figure out which configuration bits are necesary.

--- core/example3/blink.c
+++ core/example3/blink.c
@@ -1,39 +1,53 @@
 #include <pic14/pic16f887.h>
 #include <stdint.h> // for uint16_t
 
-// compile using:
-// sdcc --use-non-free -mpic14 -p16f887 blink.c
-//
-// Load to PIC:
-// pk2cmd -p pic16f887 -f blink.hex -m
-//
-// Run the program in PIC:
-// pk2cmd -p pic16f887 -A3 -T
-//
-// Or load and run at the same time:
-// pk2cmd -p pic16f887 -f blink.hex -m -A4 -T
+/*
+Wirtten on Thursday December 3rd, 2015.
+Blinks the LEDs on the 44-pin demo board populated with PIC16F887.
 
-// It is not necessary to configure _CONFIG2. This line is not tested.
+Compile using:
+sdcc --use-non-free -mpic14 -p16f887 blink.c
+
+Source configuration before using pk2cmd:
+source ../../config/environment.bashrc
+
+Load to PIC:
+pk2cmd -p pic16f887 -f blink.hex -m
+
+Run the program in PIC:
+pk2cmd -p pic16f887 -A3 -T
+
+Or load and run at the same time:
+pk2cmd -p pic16f887 -f blink.hex -m -A4 -T
+
+To power down the MCU:
+pk2cmd -p pic16f887 -R
+*/
+
+//It is not necessary to configure _CONFIG2. This line is not tested.
 //static __code uint16_t __at (_CONFIG2) configword2 = 0x2FFF; 
     
 // "static __code" is not necessary.
+// sung use cmake
+// sung modifiy .gitignore so that we only include blink.c
+// sung create build directory
+// sung contain the confiugration in a separate header.
 static __code uint16_t __at _CONFIG1 configword1 = 
-    _INTRC_OSC_NOCLKOUT & 
-    _WDT_OFF & 
-    _PWRTE_OFF & 
-    _MCLRE_OFF & 
-    _CP_OFF & 
-    _BOR_OFF & 
-    _IESO_OFF & 
-    _FCMEN_OFF; 
+    _BOR_OFF &              // needed: Brown-out Reset.Brown-out detection NOT used
+    _INTRC_OSC_NOCLKOUT &   // needed
+    _MCLRE_OFF;             // needed: Internal MCLR.  RA3 is digital input
+    //_CP_OFF &             // not a must
+    //_FCMEN_OFF &          // not a must: Fail-safe clock monitor disabled.
+    //_IESO_OFF &           // not a must: Clock switchover disabled (locked on Internal clock).
+    //_PWRTE_OFF &          // not a must: Enable power-up delay timer. Probably not necessary, but it can't hurt.
+    //_WDT_OFF;             // not a must: Watchdog Timer disabled.
 
 // It doesn't work with only these two configuration options.
 //static __code uint16_t __at (_CONFIG1) configword1 = 
 //    _WDTE_OFF &
 //    _LVP_OFF;
 
-int i;
-
+// sung try using c++11 chrono
 void delay() 
 {
     int counter = 0;
@@ -44,6 +58,8 @@ void delay()
 
 void main(void)
 {
+    // One example say (sung get url) these bits are necessary, but my
+    // application doesn't seem to require it:
     // Set ANSEL ANSELH to 0 to allow digital I/O
     //ANSEL = 0x00;
     //ANSELH = 0x00;
