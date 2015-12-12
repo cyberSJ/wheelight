055b65c45b84c4456225d0ef795353c57142d094
cyberSJ <sungjoonkim4.0@gmail.com>
2015-12-12 14:23:51 -0500

    use_pickit2 : Ignore generated files.

--- core/example3/blink.c
+++ core/example3/blink.c
@@ -29,7 +29,6 @@ pk2cmd -p pic16f887 -R
     
 // "static __code" is not necessary.
 // sung use cmake
-// sung modifiy .gitignore so that we only include blink.c
 // sung create build directory
 // sung contain the confiugration in a separate header.
 static __code uint16_t __at _CONFIG1 configword1 = 
